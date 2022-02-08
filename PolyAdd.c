0651551162eaafdfc3601f654668c7d3b29c5699
Jayasankar Shyam <84973869+jayasankar-shyam@users.noreply.github.com>
2022-02-08 11:27:09 +0530

    Create PolyAdd.c

---
+++ PolyAdd.c
@@ -0,0 +1,119 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+  {
+   int coeff;
+   int pow;
+   struct Node* next;
+  };
+
+void readPolynomial(struct Node** poly)
+  {
+   int coeff,exp,cont;
+   struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+   *poly=temp;
+
+   do
+    {
+     printf("\nEnter Coeffecient: ");
+     scanf("%d",&coeff);
+     printf("\nEnter Exponent: ");
+     scanf("%d",&exp);
+     temp->coeff=coeff;
+     temp->pow=exp;
+     temp->next=NULL;
+     printf("\nTo enter next term \nEnter 1 for YES and 0 for NO: ");
+     scanf("%d",&cont);
+     if(cont)
+      {
+       temp->next =(struct Node*)malloc(sizeof(struct Node));
+       temp=temp->next;
+       temp->next=NULL;
+      }
+    }while(cont);	
+  }
+
+void displayPolynomial(struct Node* poly)
+ {
+   printf("\nPolynomial expression is: ");
+   while(poly!=NULL)
+   {
+    printf("%dX^%d",poly->coeff,poly->pow);
+    poly=poly->next;
+    if(poly!=NULL)
+    printf("+");
+   }
+ } 
+
+void addPolynomials(struct Node** result,struct Node* first,struct Node* second)
+  {
+   struct Node* temp =(struct Node*)malloc(sizeof(struct Node));
+   temp->next=NULL;
+   *result=temp;
+   
+   while(first && second)
+     {
+      if(first->pow>second->pow)
+ 	{
+ 	 temp->coeff=first->coeff;
+ 	 temp->pow=first->pow;
+ 	 first=first->next;
+ 	}
+      else if(first->pow<second->pow)
+ 	{
+ 	 temp->coeff=second->coeff;
+ 	 temp->pow=second->pow;
+ 	 second=second->next;
+ 	}
+      else
+ 	{
+ 	 temp->coeff=first->coeff+second->coeff;
+ 	 temp->pow=first->pow;
+ 	 first=first->next;
+ 	 second=second->next;
+ 	}
+      if(first && second)
+ 	{
+ 	 temp->next=(struct Node*)malloc(sizeof(struct Node));
+ 	 temp=temp->next;
+ 	 temp->next=NULL;
+ 	}
+     }
+   while(first||second)
+     {
+      temp->next=(struct Node*)malloc(sizeof(struct Node));
+      temp=temp->next;
+      temp->next=NULL;
+ 			
+      if(first)
+ 	{
+ 	 temp->coeff=first->coeff;
+ 	 temp->pow=first->pow;
+ 	 first=first->next;
+ 	}
+      else if(second)
+ 	{
+ 	 temp->coeff=second->coeff;
+ 	 temp->pow=second->pow;
+ 	 second=second->next;
+ 	}
+     }
+  }
+
+int main()
+  {
+   struct Node*first=NULL;
+   struct Node*second=NULL;
+   struct Node*result=NULL;
+   printf("\nPolynomial Addition Using Linked List\n");
+   printf("\nFirst polynomial:\n");
+   readPolynomial(&first);
+   displayPolynomial(first);
+   printf("\nSecond polynomial:\n");
+   readPolynomial(&second);
+   displayPolynomial(second);
+   addPolynomials(&result, first, second);
+   displayPolynomial(result);
+   return 0;
+  }
